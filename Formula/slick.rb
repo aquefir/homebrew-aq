fbbe3ae4ecc39da4bd5c79484479718e689c9d3f
Alexander Nicholi <me@nicholatian.com>
2021-03-03 13:11:31 -0500

    Initial commit

---
+++ Formula/slick.rb
@@ -0,0 +1,14 @@
+##############################################################################
+##                           Aquefir Homebrew Tap                           ##
+##                                                                          ##
+##                         Copyright © 2021 Aquefir                         ##
+##                Released under Mozilla Public License 2.0.                ##
+##############################################################################
+
+class Slick < Formula
+	desc 'Just Slick.'
+	homepage 'https://aquefir.co/adp4'
+	url 'https://github.com/aquefir/slick/archive/mk-v1.3.4.tar.gz'
+	sha256 '4702e52c0d3fd364ba721fe02b0b2e3a11e5a8b82576dde6522c077d9ee31a67'
+	license 'BSD'
+	end
